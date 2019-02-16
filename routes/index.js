d47bf8851226a1f88902fecb41469f655e55d06b
Abirami <abiramirobertkennedy@gmail.com>
2019-02-16 14:47:59 -0800

    Updated Contact panel

--- routes/index.js
+++ routes/index.js
@@ -9,27 +9,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });       //Redirects to index.ejs
+  res.render('index', { title: 'Home' });       //Redirects to home page
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About me' });    //Redirects to index.ejs
+  res.render('index', { title: 'About me' });    //Redirects to About me page
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'My projects' });  //Redirects to index.ejs
+  res.render('index', { title: 'My projects' });  //Redirects to My projects page
 });
 
 /* GET service page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services offered' }); //Redirects to index.ejs
+  res.render('index', { title: 'Services offered' }); //Redirects to Services Offered page
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'Contact me' });   //Redirects to contact.ejs
+  res.render('contact', { title: 'Contact me' });   //Redirects to contact page
 });
 
 module.exports = router;
